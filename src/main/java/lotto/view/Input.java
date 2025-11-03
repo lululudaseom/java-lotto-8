caa5a033cc3a79b750b7c6592c9640c96ce8d271
jeongdaseom <lululudasum@gmail.com>
2025-11-03 17:02:26 +0900

    view-input "전체적인 틀 잡음"

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -5,12 +5,16 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Input {
-    public static int inputAmount() {
+    public static int inputAmount() { // 로또 구입 금액 입력
         return Integer.parseInt(Console.readLine());
     }
 
-    public static int inputNumbers(List<Integer> numbers) {
+    public static int inputNumbers(List<Integer> numbers) { // 당첨 번호 입력
+        return Integer.parseInt(Console.readLine());
+    }
 
+    public static int inputBonus(List<Integer> numbers) { // 보너스 번호 입력
+        return Integer.parseInt(Console.readLine());
     }
 }
 
